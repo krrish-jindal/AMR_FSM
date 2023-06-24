859351f54689c2c936e33081a360435cbbec45f9
krrish-jindal <krrishjindal2002@gmail.com>
2023-06-24 20:20:02 +0530

    add global_planner and pcl filter

--- my_pcl_tutorial/src/pcl.cpp
+++ my_pcl_tutorial/src/pcl.cpp
@@ -2,6 +2,7 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/passthrough.h>
 
 ros::Publisher pub;
@@ -12,21 +13,27 @@ void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::fromROSMsg(*cloud_msg, *cloud);
 
-  // Perform pass-through filtering
+  // Create a voxel grid filter
+  pcl::VoxelGrid<pcl::PointXYZRGB> voxelGrid;
+  voxelGrid.setInputCloud(cloud);
+  voxelGrid.setLeafSize(0.02, 0.02, 0.02);  // Specify the size of the voxels
+  voxelGrid.filter(*cloud);
+
+  // Perform pass-through filtering 
   pcl::PassThrough<pcl::PointXYZRGB> passZ;
   passZ.setInputCloud(cloud);
   passZ.setFilterFieldName("z");
-  passZ.setFilterLimits(-1, 3.0);  // Specify the range along the Z-axis to keep points
+  passZ.setFilterLimits(-1, 5.0);  // FRONT X AXIS Specify the range along the Z-axis to keep points 
   passZ.filter(*cloud);
   pcl::PassThrough<pcl::PointXYZRGB> passX;
   passX.setInputCloud(cloud);
   passX.setFilterFieldName("x");
-  passX.setFilterLimits(-5, 5.0);  // Specify the range along the Z-axis to keep points
+  passX.setFilterLimits(-5, 5.0);  // Specify the range along the X-axis to keep points
   passX.filter(*cloud);
   pcl::PassThrough<pcl::PointXYZRGB> passY;
   passY.setInputCloud(cloud);
   passY.setFilterFieldName("y");
-  passY.setFilterLimits(-2, 0.1);  // Specify the range along the Z-axis to keep points
+  passY.setFilterLimits(-0.3, 0.1);  // FRONT Z AXIS Specify the range along the Y-axis to keep points
   passY.filter(*cloud);
 
   // Convert filtered point cloud back to ROS message
@@ -39,7 +46,7 @@ void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "pass_through_filter");
+  ros::init(argc, argv, "voxel_grid_filter");
   ros::NodeHandle nh;
 
   // Create a publisher to publish the filtered point cloud
