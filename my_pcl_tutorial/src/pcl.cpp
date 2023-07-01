d85ade28f0f8a0d68afdf01bffee3d7c4af7717d
krrish-jindal <krrishjindal2002@gmail.com>
2023-07-01 14:11:30 +0530

    scale cloud data

--- my_pcl_tutorial/src/pcl.cpp
+++ my_pcl_tutorial/src/pcl.cpp
@@ -6,34 +6,43 @@
 #include <pcl/filters/passthrough.h>
 
 ros::Publisher pub;
+double scale_factor = 1.1;  // Scaling factor
 
 void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
   // Convert ROS PointCloud2 message to PCL point cloud
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::fromROSMsg(*cloud_msg, *cloud);
+  for (size_t i = 0; i < cloud->points.size(); ++i) {
+    cloud->points[i].x *= scale_factor;
+    cloud->points[i].y *= scale_factor;
+    cloud->points[i].z *= scale_factor;
+  }
   // Create a voxel grid filter
   pcl::VoxelGrid<pcl::PointXYZRGB> voxelGrid;
   voxelGrid.setInputCloud(cloud);
   voxelGrid.setLeafSize(0.02, 0.02, 0.02);  // Specify the size of the voxels
   voxelGrid.filter(*cloud);
 
+
+//   SET POINT CLOUD VISIBLE FRAME 
+
   // Perform pass-through filtering 
-  pcl::PassThrough<pcl::PointXYZRGB> passZ;
-  passZ.setInputCloud(cloud);
-  passZ.setFilterFieldName("z");
-  passZ.setFilterLimits(-1, 5.0);  // FRONT X AXIS Specify the range along the Z-axis to keep points 
-  passZ.filter(*cloud);
-  pcl::PassThrough<pcl::PointXYZRGB> passX;
-  passX.setInputCloud(cloud);
-  passX.setFilterFieldName("x");
-  passX.setFilterLimits(-5, 5.0);  // Specify the range along the X-axis to keep points
-  passX.filter(*cloud);
-  pcl::PassThrough<pcl::PointXYZRGB> passY;
-  passY.setInputCloud(cloud);
-  passY.setFilterFieldName("y");
-  passY.setFilterLimits(-0.1, 0.15);  // FRONT Z AXIS Specify the range along the Y-axis to keep points
-  passY.filter(*cloud);
+  // pcl::PassThrough<pcl::PointXYZRGB> passZ;
+  // passZ.setInputCloud(cloud);
+  // passZ.setFilterFieldName("z");
+  // passZ.setFilterLimits(-1, 5.0);  // FRONT X AXIS Specify the range along the Z-axis to keep points 
+  // passZ.filter(*cloud);
+  // pcl::PassThrough<pcl::PointXYZRGB> passX;
+  // passX.setInputCloud(cloud);
+  // passX.setFilterFieldName("x");
+  // passX.setFilterLimits(-5, 5.0);  // Specify the range along the X-axis to keep points
+  // passX.filter(*cloud);
+  // pcl::PassThrough<pcl::PointXYZRGB> passY;
+  // passY.setInputCloud(cloud);
+  // passY.setFilterFieldName("y");
+  // passY.setFilterLimits(-0.1, 0.15);  // FRONT Z AXIS Specify the range along the Y-axis to keep points
+  // passY.filter(*cloud);
 
   // Convert filtered point cloud back to ROS message
   sensor_msgs::PointCloud2 filtered_cloud_msg;
